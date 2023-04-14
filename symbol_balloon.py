c004d277132d015768fa49d18e3ba0aed9a925bc
matsukido <123632250+matsukido@users.noreply.github.com>
2023-04-14 21:59:54 +0900

    Add files via upload

--- symbol_balloon.py
+++ symbol_balloon.py
@@ -26,26 +26,26 @@ def scan_manager(scanlines):
 
     def _scan_manager_(view, start_point, end_point):
 
-        stopper = iter(range(Pkg.settings.get("max_scan_lines", 20000)))
+        stopper = iter(range(1, Pkg.settings.get("max_scan_lines", 20000)))
 
         sym_pts = Cache.views["symbol_point"]
-        idx = sym_pts.index(start_point)
-        sympts = itools.takewhile(lambda pt: pt < end_point, sym_pts[idx:])
+        index = sym_pts.index(start_point)
+        sympts = itools.takewhile(lambda pt: pt < end_point, sym_pts[index:])
         
-        zipped = zip(Cache.views["scanned_point"][idx:], 
-                     Cache.views["symbol_point"][idx + 1:],
+        zipped = zip(Cache.views["scanned_point"][index:], 
+                     sym_pts[index + 1:],
                      sympts,
-                     Cache.views["symbol_level"][idx:])
+                     Cache.views["symbol_level"][index:],
+                     itools.count(start=index))
 
-        for scanpt, nextsym, sympt, symlvl in zipped:
+        for scanpt, nextsym, sympt, symlvl, idx in zipped:
 
             delta_rgn = view.full_line(sublime.Region(scanpt, min(nextsym, end_point)))
-            start_row = 2 if scanpt == sympt else 0
-
             fulllines = view.substr(delta_rgn).splitlines(True)
 
             linestart_pts = itools.accumulate(map(len, fulllines), initial=delta_rgn.a)
 
+            start_row = 2 if scanpt == sympt else 0
             zp = itools.islice(zip(linestart_pts, fulllines, stopper), start_row, None)
             line_tpls = ((pt, lin)  for pt, lin, _ in zp if not lin == "\n")
 
@@ -55,7 +55,7 @@ def scan_manager(scanlines):
                 Cache.views["scanned_point"][idx] = new_scannedpt
                 Cache.views["closed"][idx].appendflat(closed)
 
-            idx += 1
+        return next(stopper, False)
 
     return _scan_manager_
 
@@ -88,7 +88,7 @@ def scan_lines(view, line_tuples, target_indentlevel):
                 continue
 
             idtwidth = linestr.index(topchr)
-            idtlvl = math.ceil(idtwidth / tabsize)
+            idtlvl = int(math.ceil(idtwidth / tabsize))
             if tgtlvl < idtlvl:
                 continue
 
@@ -141,7 +141,7 @@ class RaiseSymbolBalloonCommand(sublime_plugin.TextCommand):
         is_source = "Markdown" not in vw.syntax().name
 
         if is_source:
-            scan_lines(vw, top_level_pt, vpoint + 1)
+            completed = scan_lines(vw, top_level_pt, vpoint + 1)
             visible_symbol, ignoredpt = Cache.sectional_view(vpoint + 1)
 
         else:
@@ -200,8 +200,9 @@ class RaiseSymbolBalloonCommand(sublime_plugin.TextCommand):
 
         symcolor = Pkg.settings.get("symbol_color", "var(--foreground)")
         fontsize = Pkg.settings.get("font_size", 0.95)
+        ballooncolor = "#dcf" if completed else "#d77"
 
-        con = (f'<body id="symbolballoon">{_stylesheet(symcolor, fontsize)}'
+        con = (f'<body id="symbolballoon">{_stylesheet(symcolor, fontsize, ballooncolor)}'
                     '<div class="arrow"></div>'
                 f'<div class="balloon">{markup}</div></body>')
 
@@ -231,7 +232,7 @@ def _annotation_html():
             '　ignored indentation</body>')
 
 
-def _stylesheet(symbol_color, font_size):
+def _stylesheet(symbol_color, font_size, balloon_color):
     return f'''
         <style>
             .noline{{
@@ -251,14 +252,14 @@ def _stylesheet(symbol_color, font_size):
                 position: relative;
                     top: 0px;
                 border-top: 0.2rem solid transparent;
-                border-left: 0.4rem solid color(#dcf blend(var(--background) 18%));
+                border-left: 0.4rem solid color({balloon_color} blend(var(--background) 18%));
                 width: 0;
                 height: 0;
                 .balloon {{
                     position:absolute;
                     display: block;
                     text-decoration: none;
-                    background-color: color(#dcf blend(var(--background) 18%));
+                    background-color: color({balloon_color} blend(var(--background) 18%));
                     padding: 0.1rem 1.2rem 0.1rem 0.3rem;
                     border-radius: 0 0.3rem 0.3rem 0.2rem;
                 }}
